1ab67692865aaadce211b35e5dc79f5931c62da7
wadadones <salvare3429@gmail.com>
2026-04-05 16:49:38 +0900

    タイムアウトを60sに変更

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,7 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 60000, // Set test timeout to 60 seconds
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
